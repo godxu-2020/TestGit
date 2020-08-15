80148ff338a0af5dd58286fac0c7c136201e130d
xx <godxu_forever@163.com>
2020-08-15 15:01:48 +0800

    测试hot_fix分支推送

--- src/main/java/zard/xx/gly/Person.java
+++ src/main/java/zard/xx/gly/Person.java
@@ -4,4 +4,14 @@ public class Person {
 	
 	// 测试hot_fix分支推送
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
